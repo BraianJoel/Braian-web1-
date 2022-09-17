b9bea49e7086094cafaf892c889441183eb50d44
BraianJoel <braaarce@gmail.com>
2022-09-17 20:14:07 -0300

    version final

--- Primer_Cuatrimestre/TPE/scripts/captcha.js
+++ Primer_Cuatrimestre/TPE/scripts/captcha.js
@@ -1,5 +1,6 @@
 "use strict"
 document.addEventListener("DOMContentLoaded", iniciarPagina);
+
 function iniciarPagina(){
     let numero1 = Math.floor(Math.random() * 11);
     let numero2 = Math.floor(Math.random() * 11);
@@ -7,7 +8,7 @@ function iniciarPagina(){
     nodoSuma.innerHTML = numero1 + " + " + numero2;
 
     let form = document.querySelector('#contacto-form');
-    form.addEventListener("submit", (e) => {
+    form.addEventListener("submit", function(e) {
         e.preventDefault();
         let nodoResultado = document.getElementById("resultado");
         let resultado = nodoResultado.value;
